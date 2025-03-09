3985efa3c861585c1a99db60cb4022587ad6d6d1
Saadisalive <snafiullah@gmail.com>
2025-03-09 19:31:12 +0500

    Add files via upload

---
+++ while loop.py
@@ -0,0 +1,10 @@
+#input the values of terms
+n = int(input("Enter the value of terms:"))
+
+sum = 0
+i = 1 #initialisa
+while i<=n: #loop will run from 1 to n
+    sum = sum+i
+    i = i+1
+    
+print("\nSum =",sum)
